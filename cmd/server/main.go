5e11fd90d66a759ac1af482967f6733c4563aaa6
Creed Haymond <creedasaurus@gmail.com>
2018-04-24 13:59:11 -0600

    fixing file handle

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -47,12 +47,13 @@ func upload(w http.ResponseWriter, r *http.Request) {
 		}
 		defer file.Close()
 		fmt.Fprintf(w, "%v", handler.Header)
-		f, err := os.OpenFile("./"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
 		defer f.Close()
 		io.Copy(f, file)
+
 	}
 }
